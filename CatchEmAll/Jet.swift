ae15654dc380571baba3014a4ba72791e3e3f95a
edmund-h <447ght@gmail.com>
2017-02-03 00:58:17 -0500

    completed and passes all tests but still cannot get all lights to light up

--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class Jet : Plane {
+    
+    override func climb() {
+        flightCheck()
+        if inFlight {
+            altitude += 0.2 * maxAltitude
+            decelerate()
+        }
+        altitudeCheck()
+    }
+    override func dive () {
+        flightCheck()
+        if inFlight {
+            altitude -= 0.2 * maxAltitude
+            accelerate()
+        }
+        altitudeCheck()
+        flightCheck()
+    }
+    
+    func afterburner () {
+        flightCheck()
+        if speed == maxSpeed {
+            speed = maxSpeed * 2
+        }
+    }
+}
--- CatchEmAll/RaceCar.swift
+++ CatchEmAll/RaceCar.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class RaceCar: Car {
+    let driver: String
+    var sponsors: [String]
+    
+    init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double, driver: String, sponsors: [String] ){
+        self.driver = driver
+        self.sponsors = sponsors
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed, transmission: transmission, cylinders: cylinders, milesPerGallon: milesPerGallon)
+    }
+    
+    override func accelerate() {
+        speed += speedIncrement * 2
+        speedCheck()
+    }
+    override func decelerate() {
+        speed -= speedIncrement * 2
+        speedCheck()
+    }
+    func driftRight () {
+        if speed > 0 {
+           heading += 90
+           speed -= speed * 0.25
+        }
+        speedCheck()
+        headingCheck()
+    }
+    func driftLeft () {
+        if speed > 0 {
+            heading -= 90
+            speed -= speed * 0.25
+        }
+        speedCheck()
+        headingCheck()
+    }
+}
